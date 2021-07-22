2c0e60dae82598066d5d7eef0b49cabf10a31fa4
HukuToc2288 <hukutoc2288@yandex.ru>
2021-07-22 10:54:33 +0300

    Initial commit

---
+++ app/src/main/java/ru/hukutoc2288/simplepermissiondispatcher/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.hukutoc2288.simplepermissiondispatcher.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.hukutoc2288.simplepermissiondispatcher.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
